ff113022fe99d45d8aef9e7509b061439b7f76bc
BigWetTits <konditer90@gmail.com>
2012-06-26 22:08:35 +0400

    Добавил проверку реферрера.

--- js.php
+++ js.php
@@ -26,7 +26,7 @@ function setQuery($query)
 	return $res;
 }
 
-//Если передан URL
+//Если передан URL, то берём контент
 if (isset($_GET['url']))
 {
 	//Получаем хостнейм
@@ -37,15 +37,6 @@ if (isset($_GET['url']))
 	//Подключаемся к базе
 	@mysql_connect('db36.valuehost.ru', 'zanzibar2_adbo', '123456') or die("Не могу соединиться с MySQL.");
 	@mysql_select_db('zanzibar2_adbo') or die("Не могу подключиться к базе.");
-	
-	try
-	{
-		//Выставляем кодировку результатов БД
-		mysql_query('SET CHARACTER SET utf8');
-		mysql_query('SET NAMES utf8');
-	}
-	catch (Exception $e)
-	{}
 	
 	//Присоединяем все ордеры к сайту, потом ко всем ордерам присоединяем боксы, потом ищем по условиям
 	$res = getQuery('SELECT orders.id, boxes.src, boxes.href, boxes.alt
@@ -277,98 +268,113 @@ if (isset($_GET['url']))
 ";
 	}
 }
+
+//Если передан ключ статистики и id ордера, то учитываем показы и клики
 elseif (isset($_GET['stat']) and isset($_GET['id']))
 {
 	//Подключаемся к базе
 	@mysql_connect('db36.valuehost.ru', 'zanzibar2_adbo', '123456') or die("Не могу соединиться с MySQL.");
 	@mysql_select_db('zanzibar2_adbo') or die("Не могу подключиться к базе.");
 	
-	try
-	{
-		//Выставляем кодировку результатов БД
-		mysql_query('SET CHARACTER SET utf8');
-		mysql_query('SET NAMES utf8');
+	//Записываем хостнейм реферрер-урла
+	preg_match('/^.*?\/\/(.*?)\/.*/', strtolower($_SERVER['HTTP_REFERER']), $hostname);
+	if (preg_match('/^www\.(.*)/' ,$hostname[1], $matches))	 $hostname = $matches[1];
+	else $hostname = $hostname[1];
 	
-	catch (Exception $e)
-	{}
-	
-	if ($_GET['stat']=='v')
-	{
-		//получаем цену просмотра, если ордер по просмотрам
-		$res = getQuery('SELECT sites.view_price, sites.user_id
-					FROM sites
-					LEFT JOIN orders
-					ON sites.id = orders.site_id
+	//Если наш заказ по этому урлу, то учитываем статистику
+	if (getQuery('SELECT sites.id
+					FROM orders
+					LEFT JOIN sites
+					ON orders.site_id = sites.id
 					WHERE
-					orders.id = '.mysql_real_escape_string($_GET['id']).'
-					AND orders.views_limit IS NOT NULL
-					AND sites.view_price > 0
-					LIMIT 1
-					;');
-		if ($res and $res['view_price'])
-		{
-			//Ставим статус закрытия ордера, если просмотры отгремели
-			setQuery('UPDATE orders
-					SET status = "4"
-					WHERE id = '.mysql_real_escape_string($_GET['id']).'
-					AND views+1>=views_limit
-					;');
-		}
-		
-		//Добавляем просмотр в базу ордера
-		setQuery('UPDATE orders
-				SET views = views+1
-				WHERE id = '.mysql_real_escape_string($_GET['id']).';');
-
-		//Обновляем ежедневную статистику или создаём запись о новом дне
-		if (!setQuery('UPDATE statistics
-					SET views = views+1
-					WHERE order_id = '.mysql_real_escape_string($_GET['id']).'
-					AND YEAR(date) = '.date('Y').'
-					AND MONTH(date) = '.date('m').'
-					AND DAY(date) = '.date('d').'
+					sites.hostname = "'.mysql_real_escape_string($hostname).'"
+					AND orders.id = '.mysql_real_escape_string($_GET['id']).'
 					LIMIT 1
 					;'))
-			setQuery('INSERT INTO statistics VALUES(NOW(), '.mysql_real_escape_string($_GET['id']).', 0, 1);');
-	}
-	elseif ($_GET['stat']=='c')
 	{
-		//получаем цену клика, если ордер по кликам
-		$res = getQuery('SELECT sites.click_price, sites.user_id
-					FROM sites
-					LEFT JOIN orders
-					ON sites.id = orders.site_id
-					WHERE
-					orders.id = '.mysql_real_escape_string($_GET['id']).'
-					AND orders.clicks_limit IS NOT NULL
-					AND sites.click_price > 0
-					LIMIT 1
-					;');
-		if ($res and $res['click_price'])
+	
+		if ($_GET['stat']=='v')
 		{
-			//Ставим статус закрытия ордера, если клики отгремели
+			//получаем цену просмотра, если ордер по просмотрам
+			$res = getQuery('SELECT sites.view_price, sites.user_id
+						FROM sites
+						LEFT JOIN orders
+						ON sites.id = orders.site_id
+						WHERE
+						orders.id = '.mysql_real_escape_string($_GET['id']).'
+						AND orders.views_limit IS NOT NULL
+						AND sites.view_price > 0
+						LIMIT 1
+						;');
+			if ($res)
+			{
+				//Ставим статус закрытия ордера, если просмотры отгремели
+				setQuery('UPDATE orders
+						SET status = "4"
+						WHERE id = '.mysql_real_escape_string($_GET['id']).'
+						AND views+1>=views_limit
+						;');
+			}
+			
+			//Добавляем просмотр в базу ордера
 			setQuery('UPDATE orders
-					SET status = "4"
-					WHERE id = '.mysql_real_escape_string($_GET['id']).'
-					AND clicks+1>=clicks_limit
-					;');
-		}
-		
-		//Добавляем клик в базу ордера
-		setQuery('UPDATE orders
-					SET clicks = clicks+1
+					SET views = views+1
 					WHERE id = '.mysql_real_escape_string($_GET['id']).';');
-		
-		//Обновляем ежедневную статистику или создаём запись о новом дне
-		if (!setQuery('UPDATE statistics
-						SET clicks = clicks+1
+
+			//Обновляем ежедневную статистику или создаём запись о новом дне
+			if (!setQuery('UPDATE statistics
+						SET views = views+1
 						WHERE order_id = '.mysql_real_escape_string($_GET['id']).'
 						AND YEAR(date) = '.date('Y').'
 						AND MONTH(date) = '.date('m').'
 						AND DAY(date) = '.date('d').'
 						LIMIT 1
 						;'))
-			setQuery('INSERT INTO statistics VALUES(NOW(), '.mysql_real_escape_string($_GET['id']).', 1, 0);');
+				setQuery('INSERT INTO statistics VALUES(NOW(), '.mysql_real_escape_string($_GET['id']).', 0, 1);');
+		}
+		elseif ($_GET['stat']=='c')
+		{
+			//получаем цену клика, если ордер по кликам
+			$res = getQuery('SELECT sites.click_price, sites.user_id
+						FROM sites
+						LEFT JOIN orders
+						ON sites.id = orders.site_id
+						WHERE
+						orders.id = '.mysql_real_escape_string($_GET['id']).'
+						AND orders.clicks_limit IS NOT NULL
+						AND sites.click_price > 0
+						LIMIT 1
+						;');
+			if ($res)
+			{
+				//Ставим статус закрытия ордера, если клики отгремели
+				setQuery('UPDATE orders
+						SET status = "4"
+						WHERE id = '.mysql_real_escape_string($_GET['id']).'
+						AND clicks+1>=clicks_limit
+						;');
+			}
+			
+			//Добавляем клик в базу ордера
+			setQuery('UPDATE orders
+						SET clicks = clicks+1
+						WHERE id = '.mysql_real_escape_string($_GET['id']).';');
+			
+			//Обновляем ежедневную статистику или создаём запись о новом дне
+			if (!setQuery('UPDATE statistics
+							SET clicks = clicks+1
+							WHERE order_id = '.mysql_real_escape_string($_GET['id']).'
+							AND YEAR(date) = '.date('Y').'
+							AND MONTH(date) = '.date('m').'
+							AND DAY(date) = '.date('d').'
+							LIMIT 1
+							;'))
+				setQuery('INSERT INTO statistics VALUES(NOW(), '.mysql_real_escape_string($_GET['id']).', 1, 0);');
+		}
+	}
+	else
+	{
+		echo 'Не пытайся нас обмануть';
 	}
 }
 ?>
